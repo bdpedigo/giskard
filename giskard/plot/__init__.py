c57518a2a0abfb8f24c8cc8d3fc52bf246889b29
bdpedigo <benjamindpedigo@gmail.com>
2021-02-07 16:17:46 -0500

    add simple histogram w deltas func

--- giskard/plot/__init__.py
+++ giskard/plot/__init__.py
@@ -1,4 +1,5 @@
 from .bar import stacked_barplot
+from .hist import histplot
 from .matrix import dissimilarity_clustermap
 from .scatter import simple_scatterplot, screeplot, textplot, simple_umap_scatterplot
 from .utils import soft_axis_off
