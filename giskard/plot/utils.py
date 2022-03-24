8f4117388964d02ee3eafa16b0ba2b6358cbca85
bdpedigo <benjamindpedigo@gmail.com>
2022-03-24 16:52:10 -0400

    stash

--- giskard/plot/utils.py
+++ giskard/plot/utils.py
@@ -6,7 +6,7 @@ import matplotlib as mpl
 
 
 def soft_axis_off(ax, top=False, bottom=False, left=False, right=False):
-    # ax.set(xlabel="", ylabel="", xticks=[], yticks=[])
+    ax.set(xlabel="", ylabel="", xticks=[], yticks=[])
     ax.spines["top"].set_visible(top)
     ax.spines["bottom"].set_visible(bottom)
     ax.spines["left"].set_visible(left)
